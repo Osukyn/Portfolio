ab8033a449064710d5dacc3b631541520a3780b3
Osukyn <osukyn@gmail.com>
2021-04-15 11:21:33 +0200

    Add routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProjectCardComponent } from './project-card/project-card.component';
 import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+
+const appRoutes: Routes = [
+  { path: 'project/:id', component: ProjectsComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectCardComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
