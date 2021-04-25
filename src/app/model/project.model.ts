6b7e0653e4502e4793af1b1ca4cce121fb0af35b
Osukyn <osukyn@gmail.com>
2021-04-25 22:33:50 +0200

    Project model updated

--- src/app/model/project.model.ts
+++ src/app/model/project.model.ts
@@ -3,21 +3,26 @@ import {Skill} from './skill.model';
 
 export class Project {
   get id(): string {
-    return this.name.toLowerCase().replace(' ', '-');
+    return this.name.toLowerCase().split(' ').join('-');
   }
   name: string;
   description: string;
   article: Article;
+  navalgroup = false;
   get route(): string {
     return `/project&id=${this.id}`;
   }
   get image(): string {
-    return `assets/img/projects/${this.id}.jpg`;
+    return `assets/img/projects/${this.navalgroup ? 'navalgroup' : this.id}.jpg`;
   }
   skills: Array<Skill>;
-  constructor(name: string, description: string, article: Article, skills?: Array<Skill>) {
+  constructor(name: string, description: string, article: Article, skills?: Array<Skill>, navalgroup?: boolean) {
     this.name = name;
     this.description = description;
     this.article = article;
+    if (skills) {
+      this.skills = skills;
+    }
+    this.navalgroup = navalgroup !== undefined ? navalgroup : this.navalgroup;
   }
 }
